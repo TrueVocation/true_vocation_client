d31580866384dbb5145670a81ed2e986195cd74b
Arsen <arsik077@mail.ru>
2022-03-08 20:09:36 +0600

    added more components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import {GridToolbars} from "./Components/gridtoolbar/GridToolbars";
 
 function App() {
   return (
     <div className="App">
-      <GridToolbars/>
+        
     </div>
   );
 }
