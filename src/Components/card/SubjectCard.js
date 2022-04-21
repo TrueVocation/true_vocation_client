34b78e29bf8302dd4c5285ab2d02b38585797e13
Arsen <arsik077@mail.ru>
2022-04-21 08:59:10 +0600

    subjects pages, slider, menu dropdown, some changes

--- src/Components/card/SubjectCard.js
+++ src/Components/card/SubjectCard.js
@@ -4,23 +4,31 @@ import {Grid} from "@mui/material";
 import {ReactComponent as Logo} from "../../images/113-atom (2).svg";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import {useNavigate} from "react-router-dom";
 
 function SubjectCard(props) {
+    const navigate = useNavigate();
+
+    const {height, width, subject} = props;
+
     return (
         <Paper variant={"elevation"} id={"paper_hover"} elevation={0}
-               style={{padding: 32, borderRadius: 8, boxShadow: "rgb(140 152 164 / 25%) 0px 3px 6px 0px"}}
-               square={true}>
-            <Box flexDirection={"column"} width={150}>
-                <Grid container flexDirection={"row"} justifyContent={"center"}>
-                    <Logo style={{alignSelf: "center", width: 120}}/>
-                </Grid>
-                <Typography variant={"h5"} fontFamily={"Inter"} textAlign={"center"}
+               style={{paddingBottom: 32, borderRadius: 8, boxShadow: "rgb(140 152 164 / 25%) 0px 3px 6px 0px",cursor:"pointer",width:width, height:height,
+               backgroundImage:`url(${subject.picture})`,
+               backgroundRepeat:"no-repeat"}}
+               square={true} onClick={()=>navigate(`/subject/${subject.id}`,true)}>
+            <Box display={"flex"} flexDirection={"column"} style={{height:"100%"}}  justifyContent={"end"}>
+                {/*<Grid container item flexDirection={"row"} justifyContent={"center"}>*/}
+                {/*    <img style={{width:"100%", height:"100%", borderTopRightRadius:8, borderTopLeftRadius:8}} src={"https://images.pexels.com/photos/68173/flash-tesla-coil-experiment-faradayscher-cage-68173.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} />*/}
+                {/*</Grid>*/}
+                <Typography variant={"h5"} fontFamily={"Inter"} textAlign={"start"}
                             style={{
-                                color: "rgb(45, 62, 74)",
+                                color: "white",
                                 marginTop: 20,
-                                fontSize: 20,
+                                marginLeft: 20,
+                                fontSize: 25,
                                 fontWeight: "bold"
-                            }}>Physics</Typography>
+                            }}>{subject.name}</Typography>
             </Box>
         </Paper>
     );
---
+++ src/Pages/Subjects/Subjects.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import {Container, Grid} from "@mui/material";
+import SearchField from "../../Components/search/SearchField";
+import Box from "@mui/material/Box";
+import CustomAnimatedComponent from "../../Components/motion/CustomAnimatedComponent";
+import SubjectCard from "../../Components/card/SubjectCard";
+import {DoDisturb} from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
+
+function Subjects(props) {
+
+    const [search, setSearch] = useState("");
+
+    const handleSearchChange = (event) =>{
+        setSearch(event.target.value);
+    }
+
+    const subjects = [
+        {name: "Physics", picture:"https://images.pexels.com/photos/60582/newton-s-cradle-balls-sphere-action-60582.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name: "Math", picture:"https://images.pexels.com/photos/6238297/pexels-photo-6238297.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name: "History", picture:"https://images.pexels.com/photos/36006/renaissance-schallaburg-figures-facade.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name: "Geography", picture:"https://images.pexels.com/photos/414916/pexels-photo-414916.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name: "Chemistry", picture: "https://images.pexels.com/photos/7722797/pexels-photo-7722797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name:"Biology",picture:"https://images.pexels.com/photos/4916455/pexels-photo-4916455.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+        {name:"English",picture:"https://images.pexels.com/photos/970516/pexels-photo-970516.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+    ]
+
+    const displaySubjects = subjects
+            .filter(subject => subject.name.toLowerCase().includes(search.toLowerCase()))
+            .map(subject => {
+                return <Grid item style={{
+                    marginLeft: 20,
+                    marginRight: 20,
+                    marginTop: 20,
+                    marginBottom:20
+
+                }}>
+                    <CustomAnimatedComponent whileHover={{scale:1.04}} style={{
+                        display: "flex",
+                        alignItems: "center"
+                    }}>
+                        <SubjectCard subject={subject} height={300} width={300} />
+                    </CustomAnimatedComponent>
+                </Grid>
+            })
+
+    return (
+        <Box style={{
+            paddingTop:32,
+            backgroundColor:"#F7F9FF",
+            marginBottom:30
+        }}>
+            <Container maxWidth={"sm"}>
+                <SearchField value={search} handleChange={handleSearchChange} />
+            </Container>
+
+            <Container maxWidth={"xl"}>
+                <Grid container display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+                    {displaySubjects.length !== 0 ?
+                    displaySubjects
+                    :
+                        <Grid item container flexDirection={"column"} justifyContent={"center"} marginTop={5} marginBottom={10}>
+                            <Typography textAlign={"center"} variant={"h2"} fontFamily={"Inter"} fontWeight={"bold"}
+                                        style={{color: "#2d3e4a", marginBottom: 10}}>Not Found 404</Typography>
+                    <DoDisturb style={{fontSize:100, alignSelf:"center"}}/>
+                        </Grid>
+                    }
+                </Grid>
+            </Container>
+
+        </Box>
+    );
+}
+
+export default Subjects;
